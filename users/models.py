8ef94d765471cef78ff1244dadfbbc3569226534
“aha99an” <a.ha99an@gmail.com>
2023-06-21 13:52:23 +0300

    feat: add Author model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class Author(AbstractUser):
+    username = models.EmailField(unique=True, max_length=255)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=200)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return str(self.id)
