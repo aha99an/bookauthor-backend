37e117c889c3e33c550f7963799fef742b515be7
“aha99an” <a.ha99an@gmail.com>
2023-06-23 06:00:22 +0300

    feat:add Page model

--- bookauthor/settings.py
+++ bookauthor/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local apps
     "users.apps.UsersConfig",
-    "books"
-    "bages"
+    "books",
+    "pages",
 ]
 
 MIDDLEWARE = [
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from books.models import Book
 
-# Create your models here.
+class Page(models.Model):
+    book=models.ForeignKey(Book, on_delete=models.CASCADE)
+    page_no=models.IntegerField()
+    content= models.CharField(max_length=1000)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.id
