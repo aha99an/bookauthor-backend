e2956501852576a8c2d8f1af8a271968e4fc482a
“aha99an” <a.ha99an@gmail.com>
2023-06-24 13:58:29 +0300

    feat: add-permissions is own the book

--- books/views.py
+++ books/views.py
@@ -3,9 +3,20 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsOwn
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'update':
+            permission_classes = [IsAuthenticated,IsOwn]
+        elif self.action == 'destroy':
+            permission_classes = [IsAuthenticated,IsOwn]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]
+
