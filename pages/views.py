10b27e291734856004a0dcef37cfa60e0d97b36e
“aha99an” <a.ha99an@gmail.com>
2023-06-24 15:40:07 +0300

    feat: setup PageViewSet

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Page
+from .serializers import PageSerializer
 
-# Create your views here.
+class PageViewSet(viewsets.ModelViewSet):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+
+    def get_queryset(self):
+        queryset = Page.objects.filter(book_id=self.kwargs['book_id'])
+        return queryset
+    
+   
