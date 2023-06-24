d60c76effb3f370b8d26965abac775f4cecd0ca7
“aha99an” <a.ha99an@gmail.com>
2023-06-24 20:30:58 +0300

    feat: add pagination

--- bookauthor/settings.py
+++ bookauthor/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
