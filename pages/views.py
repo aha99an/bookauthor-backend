d7e8b3eb6b7d420acd293726dedb5ebbc40d3f35
“aha99an” <a.ha99an@gmail.com>
2023-06-24 18:21:31 +0300

    feat:add permitions for create, update and destory

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Page
 from .serializers import PageSerializer
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsOwnPage
+from rest_framework.response import Response
 
 class PageViewSet(viewsets.ModelViewSet):
     queryset = Page.objects.all()
     serializer_class = PageSerializer
 
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [IsAuthenticated,IsOwnPage]
+        elif self.action == 'update':
+            permission_classes = [IsAuthenticated,IsOwnPage]
+        elif self.action == 'destroy':
+            permission_classes = [IsAuthenticated,IsOwnPage]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]
+    
     def get_queryset(self):
         queryset = Page.objects.filter(book_id=self.kwargs['book_id'])
         return queryset
-    
    
+   
+
+
--- users/permissions.py
+++ users/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import BasePermission
-
+from books.models import Book
 
 class IsOwnBook(BasePermission):
     """
@@ -11,3 +11,19 @@ class IsOwnBook(BasePermission):
             return True
         return False
 
+
+
+
+class IsOwnPage(BasePermission):
+    """
+    Allows users if they are Author for this book
+    """
+    def has_permission(self, request, view):
+        book_id = view.kwargs.get('book_id')
+        book = Book.objects.filter(id=book_id, author=request.user).exists()
+        return book
+    
+    def has_object_permission(self, request, view, obj):
+        if obj.book.author.id == request.user.id:
+            return True
+        return False
